43d5de767c5c89ea03db4397960205b940baa3c1
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-30 16:51:17 +0200

    simple rspec example

---
+++ spec/rspec_spec.rb
@@ -0,0 +1,23 @@
+require 'selenium-webdriver'
+
+RSpec.describe 'Main Page' do
+  before(:example) do
+    # use MediaWiki Vagrant when running tests locally
+    # use beta cluster when running in CI
+    @url = if ENV['CI'] == 'true'
+             'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page'
+           else
+             'http://127.0.0.1:8080/wiki/Main_Page'
+           end
+    @driver = Selenium::WebDriver.for :firefox
+    @driver.navigate.to @url
+  end
+
+  it 'should have "Log in" link' do
+    expect(@driver.find_element(:link_text, 'Log in').displayed?).to be true
+  end
+
+  after(:example) do
+    @driver.quit
+  end
+end
