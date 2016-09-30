bb2fae5b697f11689778f2acbe6e7217216327c8
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-30 14:13:29 +0200

    fix rubocop offenses

--- selenium.rb
+++ selenium.rb
@@ -1,5 +1,5 @@
-require "selenium-webdriver"
-url = "http://127.0.0.1:8080/wiki/Main_Page"
+require 'selenium-webdriver'
+url = 'http://127.0.0.1:8080/wiki/Main_Page'
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to url
 puts driver.find_element(:link_text, 'Log in').displayed?
