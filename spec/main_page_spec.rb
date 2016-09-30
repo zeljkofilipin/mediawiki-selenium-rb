0e0c9cdb20918167f7d7af31b15cafb77daa37fa
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-30 18:03:54 +0200

    whitespace cleanup (rubocop)

--- spec/main_page_spec.rb
+++ spec/main_page_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe 'Main Page' do
     # use MediaWiki Vagrant when running tests locally
     # use beta cluster when running in CI
     url = if ENV['CI'] == 'true'
-             'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page'
-           else
-             'http://127.0.0.1:8080/wiki/Main_Page'
-           end
+            'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page'
+          else
+            'http://127.0.0.1:8080/wiki/Main_Page'
+          end
     @driver = Selenium::WebDriver.for :firefox
     @driver.navigate.to url
   end
