f6ea0c7a0bc09a33a2d5ad7e47b5c849cfd21ba9
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-30 17:47:59 +0200

    no need for instance variable @url in rspec example
    It can be a local variable, it is not used outside of before block.

--- spec/main_page_spec.rb
+++ spec/main_page_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'Main Page' do
   before(:example) do
     # use MediaWiki Vagrant when running tests locally
     # use beta cluster when running in CI
-    @url = if ENV['CI'] == 'true'
+    url = if ENV['CI'] == 'true'
              'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page'
            else
              'http://127.0.0.1:8080/wiki/Main_Page'
            end
     @driver = Selenium::WebDriver.for :firefox
-    @driver.navigate.to @url
+    @driver.navigate.to url
   end
 
   it 'should have "Log in" link' do
