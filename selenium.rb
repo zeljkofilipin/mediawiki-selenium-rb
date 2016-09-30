ca0911e719e26b00e14368e59af0afdc15fa44c4
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-30 14:10:12 +0200

    extract url variable

--- selenium.rb
+++ selenium.rb
@@ -1,5 +1,6 @@
 require "selenium-webdriver"
+url = "http://127.0.0.1:8080/wiki/Main_Page"
 driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://127.0.0.1:8080/wiki/Main_Page"
 puts driver.find_element(:link_text, 'Edit').displayed?
+driver.navigate.to url
 driver.quit
