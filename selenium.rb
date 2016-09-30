ed9b70dbd161fb65c56e5608d49973d621c6e4ad
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-30 16:23:20 +0200

    use beta cluster when running in CI
    use MediaWiki Vagrant when running tests locally

--- selenium.rb
+++ selenium.rb
@@ -1,5 +1,13 @@
 require 'selenium-webdriver'
-url = 'http://127.0.0.1:8080/wiki/Main_Page'
+
+# use MediaWiki Vagrant when running tests locally
+# use beta cluster when running in CI
+url = if ENV['CI'] == 'true'
+        'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page'
+      else
+        'http://127.0.0.1:8080/wiki/Main_Page'
+      end
+
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to url
 puts driver.find_element(:link_text, 'Log in').displayed?
