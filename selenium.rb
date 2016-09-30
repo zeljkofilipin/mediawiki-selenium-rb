4a1b75b668bbaaf627935c3edb6cdf05881c5f88
Željko Filipin <zeljko.filipin@gmail.com>
2016-09-30 14:10:38 +0200

    check for "log in" link

--- selenium.rb
+++ selenium.rb
@@ -1,6 +1,6 @@
 require "selenium-webdriver"
 url = "http://127.0.0.1:8080/wiki/Main_Page"
 driver = Selenium::WebDriver.for :firefox
-puts driver.find_element(:link_text, 'Edit').displayed?
 driver.navigate.to url
+puts driver.find_element(:link_text, 'Log in').displayed?
 driver.quit
